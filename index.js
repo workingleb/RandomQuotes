0a9035a63b8cd313129917b113c78758c0ebbf44
Gleb Tomchuk <workingleb@gmail.com>
2025-02-17 17:53:24 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const quotes = [
+  'The only way to do great work is to love what you do. - Steve Jobs',
+  'Innovation distinguishes between a leader and a follower. - Steve Jobs',
+  "Your time is limited, so don't waste it living someone else's life. - Steve Jobs",
+];
+
+const quoteElement = document.getElementById('quote');
+const generateBtn = document.getElementById('generate-btn');
+
+const generateRandomQuote = () => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+  quoteElement.textContent = randomQuote;
+};
+
+generateBtn.addEventListener('click', generateRandomQuote);
+
+// generateRandomQuote();
