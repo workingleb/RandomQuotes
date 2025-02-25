55d1f893fdbaf223435d89fab01ca710b1d18228
Gleb Tomchuk <workingleb@gmail.com>
2025-02-25 21:22:26 +0300

    splitted app to JS modules

--- index.js
+++ index.js
@@ -1,47 +1,4 @@
-const quotes = [
-  {
-    quote: 'The only way to do great work is to love what you do.',
-    author: 'Steve Jobs',
-  },
-  {
-    quote: 'Innovation distinguishes between a leader and a follower.',
-    author: 'Steve Jobs',
-  },
-  {
-    quote:
-      "Your time is limited, so don't waste it living someone else's life.",
-    author: 'Steve Jobs',
-  },
-  {
-    quote:
-      'Success is not final, failure is not fatal: it is the courage to continue that counts.',
-    author: 'Winston Churchill',
-  },
-  {
-    quote: 'Do what you can, with what you have, where you are.',
-    author: 'Theodore Roosevelt',
-  },
-  {
-    quote: 'In the middle of every difficulty lies opportunity.',
-    author: 'Albert Einstein',
-  },
-  {
-    quote: "Believe you can and you're halfway there.",
-    author: 'Theodore Roosevelt',
-  },
-  {
-    quote: 'It does not matter how slowly you go as long as you do not stop.',
-    author: 'Confucius',
-  },
-  {
-    quote: 'Happiness depends upon ourselves.',
-    author: 'Aristotle',
-  },
-  {
-    quote: "Opportunities don't happen. You create them.",
-    author: 'Chris Grosser',
-  },
-];
+import quotes from './quotes.js';
 
 const quoteTextElement = document.getElementById('quote');
 const quoteAuthorElement = document.getElementById('author');
