b7bee4322ae0cfafe7d35f21eda66b9f5c5409fa
Gleb Tomchuk <workingleb@gmail.com>
2025-03-01 17:29:06 +0300

    add functionality to show favorite cards

--- index.js
+++ index.js
@@ -3,13 +3,61 @@ import quotes from './quotes.js';
 const quoteTextElement = document.getElementById('quote');
 const quoteAuthorElement = document.getElementById('author');
 const generateBtn = document.getElementById('generate-btn');
+const toogleFavoriteBtn = document.getElementById('toogle-favorite-btn');
+const favoritesContainer = document.getElementById('favorites-container');
+
+let randomIndex;
 
 const generateRandomQuote = () => {
-  const randomIndex = Math.floor(Math.random() * quotes.length);
+  if (toogleFavoriteBtn.hasAttribute('disabled')) {
+    toogleFavoriteBtn.removeAttribute('disabled');
+  }
+  randomIndex = Math.floor(Math.random() * quotes.length);
   const randomQuote = quotes[randomIndex];
   const { quote: quoteText, author: quoteAuthor } = randomQuote;
   quoteTextElement.innerHTML = `"${quoteText}"`;
   quoteAuthorElement.textContent = quoteAuthor;
+  // if (quotes[randomIndex].isFavorite === true) {
+  //   toogleFavoriteBtn.textContent = 'Remove from favorites';
+  // } else {
+  //   toogleFavoriteBtn.textContent = 'Add to favorites';
+  // }
+  toogleFavoriteBtn.textContent =
+    quotes[randomIndex].isFavorite === true
+      ? 'Remove from favorites'
+      : 'Add to favorites';
+};
+
+const toogleFavorite = () => {
+  quotes[randomIndex].isFavorite = !quotes[randomIndex].isFavorite;
+  console.log(randomIndex);
+  console.log(quotes[randomIndex].isFavorite);
+  // if (quotes[randomIndex].isFavorite === true) {
+  //   toogleFavoriteBtn.textContent = 'Remove from favorites';
+  // } else {
+  //   toogleFavoriteBtn.textContent = 'Add to favorites';
+  // }
+  toogleFavoriteBtn.textContent =
+    quotes[randomIndex].isFavorite === true
+      ? 'Remove from favorites'
+      : 'Add to favorites';
+  if (quotes[randomIndex].isFavorite === true) {
+    const favoriteCard = document.createElement('div');
+    favoriteCard.classList.add('favorite-card');
+    favoriteCard.innerHTML = `
+      <p>${quotes[randomIndex].quote}</p>
+      <p class="author">${quotes[randomIndex].author}</p>
+    `;
+    favoritesContainer.appendChild(favoriteCard);
+  } else {
+    const favoriteCards = document.querySelectorAll('.favorite-card');
+    favoriteCards.forEach((card) => {
+      if (card.textContent.includes(quotes[randomIndex].quote)) {
+        card.remove();
+      }
+    });
+  }
 };
 
 generateBtn.addEventListener('click', generateRandomQuote);
+toogleFavoriteBtn.addEventListener('click', toogleFavorite);
--- quotes.js
+++ quotes.js
@@ -2,44 +2,54 @@ const quotes = [
   {
     quote: 'The only way to do great work is to love what you do.',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote: 'Innovation distinguishes between a leader and a follower.',
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote:
       "Your time is limited, so don't waste it living someone else's life.",
     author: 'Steve Jobs',
+    isFavorite: false,
   },
   {
     quote:
       'Success is not final, failure is not fatal: it is the courage to continue that counts.',
     author: 'Winston Churchill',
+    isFavorite: false,
   },
   {
     quote: 'Do what you can, with what you have, where you are.',
     author: 'Theodore Roosevelt',
+    isFavorite: false,
   },
   {
     quote: 'In the middle of every difficulty lies opportunity.',
     author: 'Albert Einstein',
+    isFavorite: false,
   },
   {
     quote: "Believe you can and you're halfway there.",
     author: 'Theodore Roosevelt',
+    isFavorite: false,
   },
   {
     quote: 'It does not matter how slowly you go as long as you do not stop.',
     author: 'Confucius',
+    isFavorite: false,
   },
   {
     quote: 'Happiness depends upon ourselves.',
     author: 'Aristotle',
+    isFavorite: false,
   },
   {
     quote: "Opportunities don't happen. You create them.",
     author: 'Chris Grosser',
+    isFavorite: false,
   },
 ];
 
